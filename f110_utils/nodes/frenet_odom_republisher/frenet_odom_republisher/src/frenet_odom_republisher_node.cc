7a47438c19aefcdd79d5a43ee2c1925b389fb3f7
Brojoon-10 <shineejoon@naver.com>
2025-11-01 17:47:58 +0000

    Fix wrapping and wall-crossing issues
    Multi-tracking fixes:
    - Fix operator precedence bug causing 10m teleportation (line 878-879)
    - Fix waypoint index calculation (line 111)
    - Unify s-center wrapping between static/dynamic obstacles (line 857)
    - Add measurement unwrapping for boundary crossings (line 563-578)
    - Add Kalman filter measurement unwrapping (line 147-165)
    
    Frenet conversion fixes:
    - Add occupancy map support for wall-crossing detection
    - Implement rotational search algorithm to avoid walls
    - Add Bresenham line algorithm for obstacle detection
    - Add OpenCV dependencies for map processing

--- f110_utils/nodes/frenet_odom_republisher/frenet_odom_republisher/src/frenet_odom_republisher_node.cc
+++ f110_utils/nodes/frenet_odom_republisher/frenet_odom_republisher/src/frenet_odom_republisher_node.cc
@@ -6,6 +6,7 @@ namespace frenet_odom_republisher{
 
 FrenetRepublisher::FrenetRepublisher(ros::NodeHandle& nh):
   nh_(nh){
+
   InitSubscribersPublishers();
   ros::spin();
 }
@@ -21,6 +22,11 @@ void FrenetRepublisher::InitSubscribersPublishers() {
 
   odom_sub_ = nh_.subscribe<nav_msgs::Odometry>
       ("/odom", 10, &FrenetRepublisher::OdomCallback, this);
+
+  // ===== HJ ADDED: Subscribe to map topic for wall filtering =====
+  map_sub_ = nh_.subscribe<nav_msgs::OccupancyGrid>
+      ("/map", 1, &FrenetRepublisher::MapCallback, this);
+  // ===== HJ ADDED END =====
 
   frenet_odom_pub_ = nh_.advertise<nav_msgs::Odometry>
       ("/odom_frenet", 1);
@@ -44,6 +50,14 @@ void FrenetRepublisher::FixedPathTrajectoryCallback(
   frenet_converter_fixed_.SetGlobalTrajectory(&fixed_path_wpt_array_, enable_wrapping);
   has_fixed_path_trajectory_ = true;
 }
+
+// ===== HJ ADDED: Map callback for wall filtering =====
+void FrenetRepublisher::MapCallback(const nav_msgs::OccupancyGridConstPtr &map_msg) {
+  ROS_INFO("[FrenetRepublisher] Received occupancy map, setting up wall filtering");
+  frenet_converter_.SetOccupancyMap(map_msg);
+  frenet_converter_fixed_.SetOccupancyMap(map_msg);
+}
+// ===== HJ ADDED END =====
 
 void FrenetRepublisher::OdomCallback(const nav_msgs::OdometryConstPtr &msg){
   // Get quaternion and yaw for both conversions
