e57647305d831af75d21bbd012f75bb5c8b000d2
HangyoCho <fins0305@gmail.com>
2026-03-16 18:32:27 +0900

    Move Livox-SDK2 build into dev_livox_setup.sh

--- .devcontainer/.install_utils/dev_livox_setup.sh
+++ .devcontainer/.install_utils/dev_livox_setup.sh
@@ -8,3 +8,9 @@ git clone --branch python3 https://github.com/pmusau17/range_libc.git
 cd ./range_libc/pywrapper
 
 python3 setup.py install
+
+# install Livox-SDK2
+git clone https://github.com/Livox-SDK/Livox-SDK2.git /tmp/Livox-SDK2
+cd /tmp/Livox-SDK2 && mkdir build && cd build
+cmake .. && make -j$(nproc) && make install
+rm -rf /tmp/Livox-SDK2
